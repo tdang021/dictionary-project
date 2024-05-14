89b5e1cba1d83ef35576e12c3431683f992480f5
tdang021 <tdang021@users.noreply.github.com>
2024-05-14 00:16:29 -0400

    Installed react and bootstrap to new project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">Hello World</div>
+      <button className="btn btn-dark">Discover</button>
+    </div>
+  );
+}
+
+export default App;
