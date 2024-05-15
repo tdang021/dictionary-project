d76fa546aa2e45875b3c7fdbb51e4b80bb35166c
tdang021 <tdang021@users.noreply.github.com>
2024-05-15 10:11:23 -0400

    Integrated axios & api dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
@@ -11,6 +12,14 @@ export default function Dictionary() {
   function search(event) {
     setKeyword(event.target.value);
   }
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
+  let apiKey = `7a6tfo3aa33dcf22944a8db00a0bf65c`;
+  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
 
   return (
     <div className="Dictionary">
