f264713d3f4acfa593908277297e4637d0a9fbc1
tdang021 <tdang021@users.noreply.github.com>
2024-05-16 01:06:50 -0400

    Rendered the word meanings (part of speech, definitions)

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleSearch(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiKey = `7a6tfo3aa33dcf22944a8db00a0bf65c`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function search(event) {
@@ -14,18 +19,17 @@ export default function Dictionary() {
   }
 
   function handleResponse(response) {
-    console.log(response);
+    setResults(response.data);
   }
-
-  let apiKey = `7a6tfo3aa33dcf22944a8db00a0bf65c`;
-  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-  axios.get(apiUrl).then(handleResponse);
 
   return (
     <div className="Dictionary">
-      <form onSubmit={handleSearch}>
-        <input type="search" autoFocus={true} onChange={search} />
-      </form>
+      <div className="Dictionary-search">
+        <form onSubmit={handleSearch}>
+          <input type="search" autoFocus={true} onChange={search} />
+        </form>
+      </div>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Meaning.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h4>{props.meaning.partOfSpeech}</h4>
+      <p>{props.meaning.definition}</p>
+    </div>
+  );
+}
