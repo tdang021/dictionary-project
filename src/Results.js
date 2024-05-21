30d323599f326bcb5a603a40728d7ed10a2a23b4
tdang021 <tdang021@users.noreply.github.com>
2024-05-21 01:04:12 -0400

    Rendered and displayed phonetics

--- src/Results.js
+++ src/Results.js
@@ -2,11 +2,14 @@ import React from "react";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
+  console.log(props);
+
   if (props.results) {
     return (
       <div className="Results">
         <br />
         <h2>{props.results.word}</h2>
+        <div className="Phonetics">{props.results.phonetic}</div>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
